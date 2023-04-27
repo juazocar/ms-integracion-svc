bd7947e906ecaff1d23fff28cdb00c8aa783469c
juazocar <ju.azocar@profesor.duoc.cl>
2023-04-27 19:16:50 -0400

    first commit

---
+++ src/main/java/cl/duoc/msintegracionsvc/controller/InstrumentoController.java
@@ -0,0 +1,10 @@
+package cl.duoc.msintegracionsvc.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/instrumentos-bff")
+public class InstrumentoController {
+    
+}
---
+++ src/test/java/cl/duoc/msintegracionsvc/MsIntegracionSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.msintegracionsvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsIntegracionSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
