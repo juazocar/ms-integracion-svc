ddba756bc4eb068475f2df5a26d094176f51887e
juazocar <ju.azocar@profesor.duoc.cl>
2023-04-27 19:54:28 -0400

    commit

--- src/main/java/cl/duoc/msintegracionsvc/controller/InstrumentoController.java
+++ src/main/java/cl/duoc/msintegracionsvc/controller/InstrumentoController.java
@@ -1,10 +1,21 @@
 package cl.duoc.msintegracionsvc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import cl.duoc.msintegracionsvc.service.InstrumentoService;
+
 @RestController
-@RequestMapping("/instrumentos-bff")
+@RequestMapping("/instrumentos-svc")
 public class InstrumentoController {
     
+    @Autowired
+    InstrumentoService instrumentoService;
+    
+    @GetMapping(path = "/saludo", produces = { "application/json" })
+    public String getSaludo(){
+        return instrumentoService.getSaludo();
+    }
 }
---
+++ src/main/java/cl/duoc/msintegracionsvc/service/InstrumentoService.java
@@ -0,0 +1,12 @@
+package cl.duoc.msintegracionsvc.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class InstrumentoService {
+    
+
+    public String getSaludo(){
+        return "HOLA SPRINGBOOT DESDE MS-INTEGRACION-SVC";
+    }
+}
